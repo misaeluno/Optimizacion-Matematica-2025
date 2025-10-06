46c5f8407f77b275270f7d49a11b6afd93ef6c4b
misaeluno <misaeluno@gmail.com>
2025-10-06 20:18:41 -0300

    arreglo ejercicio 2 alfa constante

--- Misael 2025_2S/Tarea 1/ejercicio_2/Tarea_1_2 alpha fijo.R
+++ Misael 2025_2S/Tarea 1/ejercicio_2/Tarea_1_2 alpha fijo.R
@@ -20,37 +20,41 @@ max_inter <- 10000
 #-----------------
 #contador
 contador <- 0
+contador_2 <- 0
+distancia_total <- 0 
 
-# Inicializar gráfico
+#-----------------
+# Configurar ventana con 2 gráficos
+par(mfrow = c(1, 2), mar = c(5, 5, 4, 2))
+
+# grafico de X y Y
 plot(x[1], x[2], xlim = c(0.25, 1.5), ylim = c(0.01,2), 
      xlab = "x", ylab="y log",
      log = "y",
-     main = "Descenso de Gradiente - Función de Rosenbrock",
+     main = "Descenso de Gradiente",
      pch = 20, col = "blue", cex = 2)
-#-----------------
-
-# Agregar leyenda
-legend("topright", legend = c("Inicio", "Trayectoria", "Final"), 
-       col = c("blue", "red", "black"), pch = 20, cex = 1)
 #-----------------
 
 for (i in 1:max_inter){
   #Cada vez que entra suma 1 para tener la cantidad de iteracion
   contador <- contador + 1
   #-----------------
+  
   #calculo del nuevo X
   x_nuevo <- x - alpha*f_gran(x)
   #-----------------
+ 
   #graficar
-  points(x_nuevo[1], x_nuevo[2], pch = 20, col = "red", cex = 2)
-  #-------------------------------------------------------------------
+  points(x_nuevo[1], x_nuevo[2], pch = 20, col = "purple", cex = 2)
+  
   # Graficar línea de trayectoria entre x y x_nuevo
   segments(x[1], x[2], x_nuevo[1], x_nuevo[2], col = "gray70", lwd = 4)
-  #-----------------
   # Calcular distancia recorrida en este paso
+  
   distancia_paso <- sqrt((x_nuevo[1] - x[1])^2 + (x_nuevo[2] - x[2])^2)
   distancia_total <- distancia_total + distancia_paso
   #-------------------------------------------------------------------
+  
   #retriccion para evitar errores
   if(norm(f_gran(x_nuevo), "2") < tolerancia | 
      norm(x_nuevo - x, "2") < tolerancia){
@@ -61,6 +65,20 @@ for (i in 1:max_inter){
   x <- x_nuevo
 }
 points(x_nuevo[1], x_nuevo[2], pch = 20, col = "black", cex = 2)
+
+plot(contador, alpha,
+     xlim = c(0, max_inter), ylim = c(0.001,0.003), 
+     xlab = "Iteraciones", ylab = "Alpha",
+     main = "Decaimiento de Alpha",
+     type = "n")
+grid()  # Gráfico vacío
+
+for (i in 1:max_inter){
+  contador_2 <- contador_2 + 1
+  points(contador_2, alpha, pch = 20, col = "red", cex = 1)
+}
+#-----------------------------------------------------------------
+
 cat("El número de iteraciones necesarias es:", contador, "\n")
 cat("El valor de alpha es:", alpha, "\n")
 cat("El punto mínimo encontrado es:", x_nuevo, "\n")
